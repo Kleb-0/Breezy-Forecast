0d6c09b0acde4d4b58fa1a688be1b6c8f62fd6c4
Calebe <shadowhilles@gmail.com>
2025-03-06 11:23:04 -0300

    fix: changed http to https protocol in the API call

--- src/components/Services/current-weather-data.jsx
+++ src/components/Services/current-weather-data.jsx
@@ -1,5 +1,5 @@
 const API_KEY = import.meta.env.VITE_API_KEY;
-const BASE_URL = "http://api.weatherapi.com/v1";
+const BASE_URL = "https://api.weatherapi.com/v1";
 
 const CurrentWeatherData = async (city) => {
   try {
